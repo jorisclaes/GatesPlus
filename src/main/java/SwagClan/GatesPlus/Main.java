741eeb64fe978631b6a8300d587b271241e10de7
Joris Claes <joris-claes@outlook.be>
2016-12-01 12:41:43 +0100

    init testing

---
+++ src/main/java/SwagClan/GatesPlus/Main.java
@@ -0,0 +1,18 @@
+package SwagClan.GatesPlus;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = Main.MODID, version = Main.VERSION)
+public class Main {
+	public static final String MODID = "GatesPlus";
+	public static final String VERSION = "0.1";
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+    	
+    }
+
+}
