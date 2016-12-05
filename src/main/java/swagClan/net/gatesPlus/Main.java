defc4174224389f6dbbea848e60c10ac2a475d29
Joris Claes <joris-claes@outlook.be>
2016-12-05 09:59:38 +0100

    command now works

--- src/main/java/swagClan/net/gatesPlus/Main.java
+++ src/main/java/swagClan/net/gatesPlus/Main.java
@@ -1,18 +1,22 @@
-package SwagClan.GatesPlus;
+package swagClan.net.gatesPlus;
 
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+import swagClan.net.gatesPlus.comand.CommandMain;
 
 @Mod(modid = Main.MODID, version = Main.VERSION)
 public class Main {
 	public static final String MODID = "GatesPlus";
 	public static final String VERSION = "0.1";
+	
+	public CommandMain comandMain = null;
 
     @EventHandler
-    public void init(FMLInitializationEvent event)
+    public void init(FMLServerStartingEvent event)
     {
-    	
+    	this.comandMain = new CommandMain(this, event);
     }
 
 }
---
+++ src/main/java/swagClan/net/gatesPlus/comand/CommandMain.java
@@ -0,0 +1,82 @@
+package swagClan.net.gatesPlus.comand;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommand;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+import scala.swing.TextComponent;
+import scala.tools.nsc.ScalaDoc.Command;
+import swagClan.net.gatesPlus.Main;
+
+public class CommandMain implements ICommand{
+	public Main main;
+	private List commands;
+	
+	public CommandMain(Main main, FMLServerStartingEvent event) {
+		this.main = main;
+		this.commands = new ArrayList();
+		this.commands.add("gatesplus");
+		this.commands.add("gp");
+		event.registerServerCommand(this);
+	}
+	
+
+	@Override
+	public int compareTo(ICommand o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getCommandName() {
+		// TODO Auto-generated method stub
+		return "gp";
+	}
+
+	@Override
+	public String getCommandUsage(ICommandSender sender) {
+		// TODO Auto-generated method stub
+		return "/gp";
+	}
+
+	@Override
+	public List<String> getCommandAliases() {
+		// TODO Auto-generated method stub
+		return this.commands;
+	}
+
+	@Override
+	public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
+		sender.addChatMessage(new TextComponentString("test"));
+	}
+
+	@Override
+	public boolean checkPermission(MinecraftServer server, ICommandSender sender) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public List<String> getTabCompletionOptions(MinecraftServer server, ICommandSender sender, String[] args,
+			BlockPos pos) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isUsernameIndex(String[] args, int index) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
